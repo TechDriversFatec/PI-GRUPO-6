b84d2ef5550d4c968a0916d304d26a964cf28dae
HelenAlevato <helen-alevato@hotmail.com>
2020-11-28 16:31:02 -0200

    Complemento do usuário para efetuar login

--- src/application/models/dao/UsuarioSQL.java
+++ src/application/models/dao/UsuarioSQL.java
@@ -1,39 +1,32 @@
 package application.models.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import application.models.ContaLuz;
 import application.models.Usuario;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 public class UsuarioSQL extends ConnectionBase {
 
-	
-	  public UsuarioSQL() {
-	 
-	  open();
-	  
-	  try { 
-		  PreparedStatement stm =
-	  conexao.prepareStatement("CREATE TABLE IF NOT EXISTS usuario ("
-		  +"id_user int PRIMARY KEY NOT NULL AUTO_INCREMENT,"
-		  + "nome_user VARCHAR(40)," 
-		  + "cpf_user VARCHAR(11)," 
-		  + "login_user VARCHAR(20)," 
-		  +"senha_user VARCHAR(20)," 
-		  + "tipo_user VARCHAR(20);"); 
-		  stm.executeUpdate();
-	  
-		stm.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			close();
-		}
+	public UsuarioSQL() {
+
+//		open();
+//
+//		try {
+//			PreparedStatement stm = conexao.prepareStatement(
+//					"CREATE TABLE IF NOT EXISTS usuario (" + "id_user int PRIMARY KEY NOT NULL AUTO_INCREMENT,"
+//							+ "nome_user VARCHAR(40)," + "cpf_user VARCHAR(11)," + "login_user VARCHAR(20),"
+//							+ "senha_user VARCHAR(20)," + "tipo_user VARCHAR(20));");
+//			stm.executeUpdate();
+//
+//			stm.executeUpdate();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} finally {
+//			close();
+//		}
 	}
 
 	public void create(Usuario user) {
@@ -64,6 +57,35 @@ public class UsuarioSQL extends ConnectionBase {
 		}
 	}
 
+	public Usuario buscarUsuarioPorLogin(String login) {
+		open();
+
+		Usuario usuario = null;
+
+		try {
+			PreparedStatement stm = conexao.prepareStatement("SELECT * FROM usuario WHERE login_user='" + login + "'");
+
+			ResultSet rs = stm.executeQuery();
+
+			if (rs != null && rs.next()) {
+				usuario = new Usuario(rs.getInt(1), // id
+						rs.getString(2), // nome
+						rs.getString(3), // cpf
+						rs.getString(4), // login
+						rs.getString(5), // senha_user
+						rs.getString(6), // tipo_user
+						rs.getString(7) // telefone_user
+				);
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro ao conectar com o banco");
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return usuario;
+	}
+
 	public List<Usuario> all() {
 
 		open();
@@ -83,7 +105,7 @@ public class UsuarioSQL extends ConnectionBase {
 						rs.getString(4), // login
 						rs.getString(5), // senha_user
 						rs.getString(6), // tipo_user
-						rs.getString(7)	 // telefone_user
+						rs.getString(7) // telefone_user
 				);
 				result.add(user);
 			}
